0abaf4f1316ef3036ac1d126046a5d972e03c68a
Mina Hawarei <83174542+MinaHawarei@users.noreply.github.com>
2024-11-10 16:09:01 +0200

    Update ElevatorModel.tsx

--- src/components/ElevatorModel.tsx
+++ src/components/ElevatorModel.tsx
@@ -1,11 +1,9 @@
 import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
-import { Mesh } from 'three';
+import {  Mesh } from 'three';
 import { ElevatorConfig } from '../App';
-import { useLoader } from '@react-three/fiber';
+import {  useLoader } from '@react-three/fiber';
 import * as THREE from 'three';
-import { Canvas } from '@react-three/fiber'
-import { SpotLight } from '@react-three/drei';
 
 interface Props {
   config: ElevatorConfig;
@@ -22,6 +20,7 @@ const FLOORL_COLORS = {
   'floor2': '/src/materials/floor/2.png',
   'floor3': '/src/materials/floor/3.png'
 } as const;
+
 
 const CEILING_COLORS = {
   'ceiling1': '/src/materials/ceiling/1.png',
@@ -41,11 +40,13 @@ const SPOT_FRAME = {
   'white': '/src/materials/frame/2.png'
 } as const;
 
+const SPOT_COLOR = {
+  'white': '#FFFFFF',
+  'warm': '#ffff00'
+} as const;
+
+
 const lightwarm = '/src/materials/lights/1.png';
-
-
-
-
 
 
 function ElevatorModel({ config }: Props) {
@@ -56,27 +57,22 @@ function ElevatorModel({ config }: Props) {
       meshRef.current.rotation.y += 0.002;
     }
   });
-
   const width = config.width / 1000;
   const height = config.height / 1000;
   const depth = config.depth / 1000;
 
   const CONTROLPANLE_POSITION = {
-    'rightCenter': [width/2-0.01, 0, 0],
-    'rightCorner': [width/2-0.01, 0, depth/2+0.01-0.2],
-    'leftCenter': [-width/2+0.01, 0, 0],
-    'leftCorner': [-width/2+0.01, 0, depth/2+0.01-0.2],
-    'frontCenter': [0 , 0, -depth/2+0.01],
-    'frontRight': [width/2-0.2, 0, -depth/2+0.01],
-    'frontLeft': [-width/2+0.2, 0, -depth/2+0.01]
+    'rightCenter': [width/2-0.02, 0, 0],
+    'rightCorner': [width/2-0.02, 0, depth/2+0.02-0.2],
+    'leftCenter': [-width/2+0.02, 0, 0],
+    'leftCorner': [-width/2+0.02, 0, depth/2+0.02-0.2],
+    'frontCenter': [0 , 0, -depth/2+0.02],
+    'frontRight': [width/2-0.2, 0, -depth/2+0.02],
+    'frontLeft': [-width/2+0.2, 0, -depth/2+0.02]
  
   } as const;
 
-
-
-
-
-
+  const spotlightHeighr = height/2-0.01;
   const wallColor1 = MATERIAL_COLORS[config.wallMaterial1];
   const wallColor2 = MATERIAL_COLORS[config.wallMaterial2];
   const wallColor3 = MATERIAL_COLORS[config.wallMaterial3];
@@ -85,25 +81,18 @@ function ElevatorModel({ config }: Props) {
   const cp =  CONTROLPANLE_STYLE[config.cpMaterial];
   const cpposition =  CONTROLPANLE_POSITION[config.cpPosition];
   const spotframe = SPOT_FRAME[config.spotFrame];
+  const spotcolor = SPOT_COLOR[config.lightType];
 
   var cpdirection ;
   if (config.cpPosition == 'leftCenter' || config.cpPosition == 'leftCorner'){
-    cpdirection = [0, Math.PI / 2, 0];
+    cpdirection = Math.PI / 2;
   }else if (config.cpPosition == 'rightCenter' || config.cpPosition == 'rightCorner'){
 
-    cpdirection = [0, -Math.PI / 2, 0];
+    cpdirection = -Math.PI / 2;
   }else{
-    cpdirection = [0,0,0];
-  }
-  
+    cpdirection = 0;
+  };
 
-
-
-
-
-
-
-  
   const floorColortest = useLoader(THREE.TextureLoader,fffloor);
   const ceilingDesign = useLoader(THREE.TextureLoader,ceiling);
   const wallDesign1 = useLoader(THREE.TextureLoader,wallColor1);
@@ -115,136 +104,165 @@ function ElevatorModel({ config }: Props) {
 
 
 
-  // Door dimensions in meters
-  const doorWidth = config.width / 1000;
-  const doorHeight = config.height / 1000;
-
+  const marbleNormalMap = useLoader(THREE.TextureLoader, 'src/materials/floor/marble/14/Marble014_1K-JPG_NormalDX.jpg'); 
+  const marbleRoughnessMap = useLoader(THREE.TextureLoader, 'src/materials/floor/marble/14/Marble014_1K-JPG_Roughness.jpg');
   return (
-    <group ref={meshRef}>
+
+    <group>
+        
       {/* Floor */}
-
-        <mesh position={[0, -height/2, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-          <planeGeometry args={[width, depth]} />
-         
-          <meshStandardMaterial map={floorColortest} />
-
+      <mesh position={[0, -height/2, 0]} rotation={[-Math.PI / 2, 0, 0]} castShadow receiveShadow>
+         <boxGeometry args={[width,depth,0.05]} />
+          <meshPhysicalMaterial
+            map={floorColortest}
+            normalMap={marbleNormalMap}
+            roughnessMap={marbleRoughnessMap}
+            roughness={0.5}
+            metalness={0}
+            reflectivity={0.9}
+            clearcoat={1}
+            clearcoatRoughness={0.1} />
       </mesh>
+    
+      
 
       {/* Ceiling */}
-      <mesh position={[0, height/2, 0]} rotation={[Math.PI / 2, 0, 0]}>
-        <planeGeometry args={[width, depth]} />
+      <mesh position={[0, height/2, 0]} rotation={[Math.PI / 2, 0, 0]} castShadow receiveShadow >
+        <boxGeometry args={[width, depth, 0.02]} />
         <meshStandardMaterial color={"#ffffff"} />
-        <meshStandardMaterial map={ceilingDesign} />
+        <meshPhysicalMaterial
+            map={ceilingDesign}
+            normalMap={marbleNormalMap}
+            roughnessMap={marbleRoughnessMap}
+            roughness={-0.5}
+            metalness={0}
+            reflectivity={0.9}
+            clearcoat={1}
+            clearcoatRoughness={0.1} />
       </mesh>
 
       {/* Back wall */}
-      {config.entranceCount === 2 ? (
+      {config.entranceCount === 1 ? (
         <>
-          {/* Left section */}
-          <mesh position={[-width/4 - doorWidth/4, 0, -depth/2]}>
-            <planeGeometry args={[(width - doorWidth)/2, height]} />
-            <meshStandardMaterial map={wallDesign1} />
+          <mesh position={[0, 0, -depth/2]}>
+          <boxGeometry args={[width, height , 0.02]} />
+            <meshPhysicalMaterial
+            map={wallDesign1}
+            normalMap={marbleNormalMap}
+            roughnessMap={marbleRoughnessMap}
+            roughness={0.5}
+            metalness={0}
+            reflectivity={0.9}
+            clearcoat={1}
+            clearcoatRoughness={0.1} />
           </mesh>
-          {/* Right section */}
-          <mesh position={[width/4 + doorWidth/4, 0, -depth/2]}>
-            <planeGeometry args={[(width - doorWidth)/2, height]} />
-            <meshStandardMaterial map={wallDesign1} />
-          </mesh>
-          {/* Top section */}
-          <mesh position={[0, height/2 - (height - doorHeight)/4, -depth/2]}>
-            <planeGeometry args={[doorWidth, (height - doorHeight)/2]} />
-            <meshStandardMaterial map={wallDesign1} />
-          </mesh>
+       
         </>
       ) : (
-        <mesh position={[0, 0, -depth/2]}>
-          <planeGeometry args={[width, height]} />
-          <meshStandardMaterial map={wallDesign1} />
-        </mesh>
+        <></>
+        
       )}
+      <mesh position={[0, 0, -depth/2+0.02]}>
+        <boxGeometry args={[0.5 , 1 , 0.005]} />
+
+
+      </mesh>
 
       {/* Side left wall */}
-      <mesh position={[-width/2, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
-        <planeGeometry args={[depth, height]} />
-        <meshStandardMaterial map={wallDesign2} />
+      <mesh position={[-width/2, 0, 0]} rotation={[0, Math.PI / 2, 0]} castShadow receiveShadow >
+        <boxGeometry args={[depth, height , 0.02]} />
+        <meshPhysicalMaterial
+            map={wallDesign2}
+            normalMap={marbleNormalMap}
+            roughnessMap={marbleRoughnessMap}
+            roughness={0.5}
+            metalness={0}
+            reflectivity={0.9}
+            clearcoat={1}
+            clearcoatRoughness={0.1} />
       </mesh>
 
       {/* Side right wall */}
       <mesh position={[width/2, 0, 0]} rotation={[0, -Math.PI / 2, 0]}>
-        <planeGeometry args={[depth, height]} />
-        <meshStandardMaterial map={wallDesign3} />
+        <boxGeometry args={[depth, height , 0.02]} />
+        <meshPhysicalMaterial
+            map={wallDesign3}
+            normalMap={marbleNormalMap}
+            roughnessMap={marbleRoughnessMap}
+            roughness={0.5}
+            metalness={0}
+            reflectivity={0.9}
+            clearcoat={1}
+            clearcoatRoughness={0.1} />
       </mesh>
 
       {/* controll panle */}
 
 
-      <mesh position={cpposition} rotation={cpdirection}>
+      <mesh position={cpposition} rotation ={[0, cpdirection , 0 ]} castShadow receiveShadow>
               <planeGeometry args={[0.2, 1.9]} />
               <meshStandardMaterial map={cpDesign} />
 
       </mesh>
       
       {/* spot lights 1 */}
-      <mesh position={[width/2-width/8, height/2-0.001, -depth/2 + width/8]} rotation={[Math.PI / 2, 0, 0]}>
+      <mesh position={[width/2-width/8, spotlightHeighr-0.005, -depth/2 + width/8]} rotation={[Math.PI / 2, 0, 0]} castShadow receiveShadow >
               <circleGeometry args={[0.05 , 100]} />
               <meshStandardMaterial map={lights} />
-
       </mesh>
-      <mesh position={[width/2-width/8, height/2-0.001, -depth/2 + width/8]} rotation={[Math.PI / 2, 0, 0]}>
-              <torusGeometry args={[0.05,0.01,3,100]} />
+      <spotLight position={[width/2-width/8, spotlightHeighr-0.005, -depth/2 + width/8]} angle={1.2} penumbra={1.3} intensity={0.5} color={spotcolor}  castShadow />
+      <pointLight position={[width/2-width/8, spotlightHeighr-0.051, -depth/2 + width/8]} intensity={0.01} color={spotcolor} castShadow />
+
+      <mesh position={[width/2-width/8, spotlightHeighr, -depth/2 + width/8]} rotation={[0, 0, 0]}>
+              <cylinderGeometry args={[0.06,0.06,0.005,100]} />
               <meshStandardMaterial map = {spotFrame} />
+              
       </mesh>
 
       {/* spot lights 2 */}
-      <mesh position={[width/2-width/8, height/2-0.001, depth/2 - width/8]} rotation={[Math.PI / 2, 0, 0]}>
+      <mesh position={[width/2-width/8, spotlightHeighr-0.005, depth/2 - width/8]} rotation={[Math.PI / 2, 0, 0]} castShadow receiveShadow>
               <circleGeometry args={[0.05 , 100]} />
               <meshStandardMaterial map={lights} />
-
+              
       </mesh>
-      <mesh position={[width/2-width/8, height/2-0.001, depth/2 - width/8]} rotation={[Math.PI / 2, 0, 0]}>
-              <torusGeometry args={[0.05,0.01,3,100]} />
+      <spotLight position={[width/2-width/8, spotlightHeighr-0.005, depth/2 - width/8]} angle={1.2} penumbra={1.3} intensity={0.5} color={spotcolor}  castShadow />
+      <pointLight position={[width/2-width/8, spotlightHeighr-0.051, depth/2 - width/8]} intensity={0.01} color={spotcolor} castShadow />
+
+
+      <mesh position={[width/2-width/8, spotlightHeighr, depth/2 - width/8]} rotation={[0, 0, 0]}>
+              <cylinderGeometry args={[0.06,0.06,0.005,100]} />
               <meshStandardMaterial map = {spotFrame} />
+            
+
       </mesh>
 
       {/* spot lights 3 */}
-            <mesh position={[-width/2+width/8, height/2-0.001, depth/2 - width/8]} rotation={[Math.PI / 2, 0, 0]}>
+            <mesh position={[-width/2+width/8, spotlightHeighr-0.005, depth/2 - width/8]} rotation={[Math.PI / 2, 0, 0]} castShadow receiveShadow>
               <circleGeometry args={[0.05 , 100]} />
               <meshStandardMaterial map={lights} />
+      </mesh>
+      <spotLight position={[-width/2+width/8, spotlightHeighr-0.005, depth/2 - width/8]} angle={1.2} penumbra={1.3} intensity={0.5} color={spotcolor}  castShadow />
+      <pointLight position={[-width/2+width/8, spotlightHeighr-0.051, depth/2 - width/8]} intensity={0.01} color={spotcolor} castShadow />
 
-      </mesh>
-      <mesh position={[-width/2+width/8, height/2-0.001, depth/2 - width/8]} rotation={[Math.PI / 2, 0, 0]}>
-              <torusGeometry args={[0.05,0.01,3,100]} />
-              <meshStandardMaterial map = {spotFrame} />
-      </mesh>
-            {/* spot lights 4 */}
-            <mesh position={[-width/2+width/8, height/2-0.001, -depth/2 + width/8]} rotation={[Math.PI / 2, 0, 0]}>
-              <circleGeometry args={[0.05 , 100]} />
-              <meshStandardMaterial map={lights} />
-
-      </mesh>
-      <mesh position={[-width/2+width/8, height/2-0.001, -depth/2 + width/8]} rotation={[Math.PI / 2, 0, 0]}>
-              <torusGeometry args={[0.05,0.01,3,100]} />
+      <mesh position={[-width/2+width/8, spotlightHeighr, depth/2 - width/8]} rotation={[0, 0, 0]}>
+              <cylinderGeometry args={[0.06,0.06,0.005,100]} />
               <meshStandardMaterial map = {spotFrame} />
       </mesh>
 
+      {/* spot lights 4 */}
+      <mesh position={[-width/2+width/8, spotlightHeighr-0.005, -depth/2 + width/8]} rotation={[Math.PI / 2, 0, 0]} castShadow receiveShadow>
+        <circleGeometry args={[0.05 , 100]} />
+        <meshStandardMaterial map={lights} />
+      </mesh>
+      <spotLight position={[-width/2+width/8, spotlightHeighr-0.005, -depth/2 + width/8]} angle={1.2} penumbra={1.3} intensity={0.5} color={spotcolor} castShadow />
+      <pointLight position={[-width/2+width/8, spotlightHeighr-0.051, -depth/2 + width/8]} intensity={0.01} color={spotcolor} castShadow />
+      <mesh position={[-width/2+width/8, spotlightHeighr, -depth/2 + width/8]} rotation={[0, 0, 0]}>
+              <cylinderGeometry args={[0.06,0.06,0.005,100]} />
+              <meshStandardMaterial map = {spotFrame} />
+      </mesh>
 
-      {/* Front wall with entrance */}
-      {/* Left section */}
-      <mesh position={[-width/4 - doorWidth/4, 0, depth/2]} rotation={[0, Math.PI, 0]}>
-        <planeGeometry args={[(width - doorWidth)/2, height]} />
-        <meshStandardMaterial map={wallDesign3} />
-      </mesh>
-      {/* Right section */}
-      <mesh position={[width/4 + doorWidth/4, 0, depth/2]} rotation={[0, Math.PI, 0]}>
-        <planeGeometry args={[(width - doorWidth)/2, height]} />
-        <meshStandardMaterial map={wallDesign3} />
-      </mesh>
-      {/* Top section */}
-      <mesh position={[0, height/2 - (height - doorHeight)/4, depth/2]} rotation={[0, Math.PI, 0]}>
-        <planeGeometry args={[doorWidth, (height - doorHeight)/2]} />
-        <meshStandardMaterial map={wallDesign3} />
-      </mesh>
     </group>
+
   );
 }
 
